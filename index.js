62d6285ad6c0e6a96945574dbd56474653884e2e
alalfakawma <alalfakawma@live.com>
2022-05-20 14:16:59 +0530

    Print server added

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
 const escpos = require("escpos");
 // install escpos-usb adapter module manually
 escpos.USB = require("escpos-usb");
-// Select the adapter based on your printer type
-const device = new escpos.USB();
-// const device  = new escpos.Network('localhost');
-// const device  = new escpos.Serial('/dev/usb/lp0');
 
-const options = { encoding: "GB18030" /* default */ };
-// encoding is optional
+function printReceipt() {
+    // Select the adapter based on your printer type
+    const device = new escpos.USB();
+    // const device  = new escpos.Network('localhost');
+    // const device  = new escpos.Serial('/dev/usb/lp0');
 
-const printer = new escpos.Printer(device, options);
+    const options = { encoding: "GB18030" /* default */ };
+    // encoding is optional
 
-device.open(function () {
-  printer
-    .font("a")
-    .align("ct")
-    .style("bu")
-    .size(1, 1)
-    .text("Rinawma Hmelchhe ber e")
-    .text("Hmelchhe bawk, zu heh bawk")
-    .barcode("1234567", "EAN8")
-    .table(["One", "Two", "Three"])
-    .tableCustom(
-      [
-        { text: "Left", align: "LEFT", width: 0.33, style: "B" },
-        { text: "Center", align: "CENTER", width: 0.33 },
-        { text: "Right", align: "RIGHT", width: 0.33 },
-      ],
-      { encoding: "cp857", size: [1, 1] } // Optional
-    )
-    .qrimage("https://github.com/song940/node-escpos", function () {
-      this.cut();
-      this.close();
+    const printer = new escpos.Printer(device, options);
+
+    device.open(function () {
+        printer
+            .font("a")
+            .align("ct")
+            .style("bu")
+            .size(1, 1)
+            .text("Rinawma Hmelchhe ber e")
+            .text("Hmelchhe bawk, zu heh bawk")
+            .barcode("1234567", "EAN8")
+            .table(["One", "Two", "Three"])
+            .tableCustom(
+                [
+                    { text: "Left", align: "LEFT", width: 0.33, style: "B" },
+                    { text: "Center", align: "CENTER", width: 0.33 },
+                    { text: "Right", align: "RIGHT", width: 0.33 },
+                ],
+                { encoding: "cp857", size: [1, 1] } // Optional
+            )
+            .qrimage("https://github.com/song940/node-escpos", function () {
+                this.cut();
+                this.close();
+            });
     });
+}
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/print-receipt", (req, res) => {
+    printReceipt();
+
+    res.sendStatus(204);
 });
+
+app.listen(3000, () => console.log("Webhook server is listening, port 3000"));
