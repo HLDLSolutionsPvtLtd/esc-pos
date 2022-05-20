e00f560b034adf3cc15d5000f8cccde8708bdc20
Lalrinawma <terinao86@gmail.com>
2022-05-20 16:51:52 +0530

    update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const parseISO = require("date-fns").parseISO;
+const format = require("date-fns").format;
 const escpos = require("escpos");
 // install escpos-usb adapter module manually
 escpos.USB = require("escpos-usb");
@@ -8,7 +10,7 @@ function printReceipt(data) {
     // const device  = new escpos.Network('localhost');
     // const device  = new escpos.Serial('/dev/usb/lp0');
 
-    const options = { encoding: "GB18030" /* default */ };
+    const options = { encoding: "UTF-8" /* default */ , width: 63};
     // encoding is optional
 
     const printer = new escpos.Printer(device, options);
@@ -17,24 +19,86 @@ function printReceipt(data) {
         printer
             .font("a")
             .align("ct")
-            .style("bu")
+            .style("b")
             .size(1, 1)
-            .text("Rinawma Hmelchhe ber e")
-            .text("Hmelchhe bawk, zu heh bawk")
-            .barcode("1234567", "EAN8")
-            .table(["One", "Two", "Three"])
+            .text("FAMCART POS RECEIPT");
+
+            printer.style("").size(0, 0).text("------------------------------------------------");
+            printer.font("b")
+            .align("lt")
+            .style("")
+            .size(0, 0)
             .tableCustom(
                 [
-                    { text: "Left", align: "LEFT", width: 0.33, style: "B" },
-                    { text: "Center", align: "CENTER", width: 0.33 },
-                    { text: "Right", align: "RIGHT", width: 0.33 },
+                  { text:"Famcart", align:"LEFT", width:0.5},
+                  { text:format(parseISO(data.created_at), 'hh:mm aaa'), align:"RIGHT", width:0.5},
                 ],
-                { encoding: "cp857", size: [1, 1] } // Optional
-            )
-            .qrimage("https://github.com/song940/node-escpos", function () {
-                this.cut();
-                this.close();
-            });
+              )
+            .tableCustom(
+                [
+                  { text:"Address: Chaltlang", align:"LEFT", width:0.5},
+                  { text: format(parseISO(data.created_at), 'dd-MM-Y'), align:"RIGHT", width:0.5},
+                ],
+              )
+
+            .tableCustom(
+                [
+                  { text:"Contact: 343463", align:"LEFT", width:0.5},
+                  { text:"Terminal #1", align:"RIGHT", width:0.5},
+                ],
+              );
+            
+            printer.drawLine();
+            printer.tableCustom(
+                [
+                  { text:"ITEM", align:"LEFT", width:0.5},
+                  { text:"PRICE", align:"RIGHT", width:0.5},
+                ],
+              );
+            data.products.forEach((element) => {
+               printer.tableCustom(
+                    [
+                    { text: element.inventory.item.name, align:"LEFT", width:0.5},
+                    { text: element.pivot.quantity+ "x" +"Rs. "+element.inventory.selling_price, align:"RIGHT", width:0.5},
+                    ],
+              );
+           })
+           printer.drawLine();
+           printer.tableCustom(
+                [
+                    { text: "SUB TOTAL", align:"LEFT", width:0.5},
+                    { text: "Rs. "+data.total_without_gst, align:"RIGHT", width:0.5},
+                ],
+            );
+            printer.tableCustom(
+                [
+                    { text: "TAX", align:"LEFT", width:0.5},
+                    { text: "Rs. "+(data.total_with_gst - data.total_without_gst).toFixed(2), align:"RIGHT", width:0.5},
+                ],
+            );
+            printer.tableCustom(
+                [
+                    { text: "TOTAL PAYABLE", align:"LEFT", width:0.5},
+                    { text: "Rs. "+data.total_with_gst, align:"RIGHT", width:0.5},
+                ],
+            );
+            
+            printer.drawLine();
+             
+            printer.font("b")
+            .align("ct")
+            .size(0, 0)
+            .text("THANK YOU FOR CHOOSING US");
+            
+            printer.font("a")
+            .align("ct")
+            .size(0, 0)
+            .text("www.famcart.in");
+
+            printer.style("").size(0, 0).text("------------------------------------------------");
+
+                printer.cut();
+                printer.close();
     });
 }
 
