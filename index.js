63e9ecc1f658492f7142208a3fdfe43996fb9206
alalfakawma <alalfakawma@live.com>
2022-05-20 14:39:44 +0530

    Update cors options

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-    origin: ["http://localhost:8000"],
+    origin: ["http://localhost:8080", "http://localhost:8000"],
 };
 
 app.use(bodyParser.json());
